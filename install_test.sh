3bf7725517a3b2bbb05c8dd8641519019fea9926
Patrick Bills <billspat@msu.edu>
2020-09-24 23:28:45 -0400

    even simpler test

--- install_test.sh
+++ install_test.sh
@@ -1,22 +1,24 @@
 #!/bin/bash
-apt-get -y update
+
 
 # set up a silent install of MySQL
 passed_var=$1
 
-export DEBIAN_FRONTEND=noninteractive
+# echo "deb https://cloud.r-project.org/bin/linux/ubuntu bionic-cran40/" >> /etc/apt/sources.list 
+# apt-get -y update
+# apt-get install r-base r-base-dev
+# export DEBIAN_FRONTEND=noninteractive
 # echo mysql-server-5.6 mysql-server/root_password password $dbpass | debconf-set-selections
 # echo mysql-server-5.6 mysql-server/root_password_again password $dbpass | debconf-set-selections
 
 # install the LAMP stack
-apt-get -y install apache2 php5 
+apt-get -y install apache2  
 
-mkdir -p /var/www/html/
+# mkdir -p /var/www/html/ 
 
-# write some PHP
-echo \<center\>\<h1\>My Demo App\</h1\>\<br/\>\</center\> > /var/www/html/index.php
-echo "<p>Passed Variable = $passed_var" >> /var/www/html/index.php
-echo \<\?php phpinfo\(\)\; \?\> >> /var/www/html/index.php
+# # write some PHP
+# echo \<center\>\<h1\>My Demo App\</h1\>\<br/\>\</center\> > /var/www/html/index.php
+# echo "<p>Passed Variable = $passed_var" >> /var/www/html/index.php
+# echo \<\?php phpinfo\(\)\; \?\> >> /var/www/html/index.php
 
-# restart Apache
 apachectl restart
