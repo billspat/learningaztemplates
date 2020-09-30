6bc5876a1f9fa2bb374397d2e444aac3ba776245
Patrick Bills <billspat@msu.edu>
2020-09-30 10:51:25 -0400

    adding VM user and  passwd ;open rstudio port

--- ubuntu-apache-test-page/scripts/installrstudio.sh
+++ ubuntu-apache-test-page/scripts/installrstudio.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+NEWUSER=rstudio
+NEWUSERPASSWORD=$1
+
 sudo apt-get update
 sudo apt-get -y upgrade
 
+# this was from  a quickstart script from Microsoft.  I'm keeping it here for ref, but  pls delete
 # sudo apt-get install -y apache2
 # if [ "$1" = "True" ]; then
 # 	if [ "$4" = "16.04.0-LTS" ]; then
@@ -17,19 +21,21 @@ sudo apt-get -y upgrade
 # fi
 # echo $2 | sudo tee /var/www/html/$3
 
+# add a regular linux user for Rstudio to work with 
+useradd NEWUSER
+echo "$NEWUSERPASSWORD" | passwd theUsername --stdin
 
 cd /
 
+# taken from https://rstudio.com/products/rstudio/download-server/debian-ubuntu/
 mkdir rstudiostartup
-
 cd rstudiostartup
-
+# do we need sudo for azure install scripts?
 sudo add-apt-repository ppa:marutter/rrutter
 sudo apt update
 
 sudo apt-get -y install r-base
 sudo apt-get -y install apache2
-
 sudo apt-get -y install gdebi-core
 
 wget https://download2.rstudio.org/server/bionic/amd64/rstudio-server-1.3.1093-amd64.deb
