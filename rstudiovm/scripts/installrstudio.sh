db3d38e06e071d3163aab1dce7031b62c402badf
Patrick Bills <billspat@msu.edu>
2020-10-02 19:14:22 -0400

    troubleshooting install script

--- rstudiovm/scripts/installrstudio.sh
+++ rstudiovm/scripts/installrstudio.sh
@@ -1,26 +1,31 @@
 #!/bin/bash
 
+# run this all as root
+# TODO - can we use sudo in the command in the template?
+sudo su
+
 export NEWUSER=rstudio
 export NEWUSERPASSWORD=$1
 export AZURE_STORAGE_ACCOUNT=$2
 export AZURE_CONTAINER=$3
 export AZURE_STORAGE_ACCESS_KEY=$4
 
-sudo apt-get update
-sudo apt-get -y upgrade
+apt-get update
+apt-get -y upgrade
 
 # add a regular linux user for Rstudio to work with 
-yes `echo $NEWUSERPASSWORD` | sudo useradd $NEWUSER  -d /home/$NEWUSER
+useradd $NEWUSER  -m
+yes $NEWUSERPASSWORD | passwd $NEWUSER
 # this is not the same as the admin user set by the Azure template, and is not in the sudoers list.  
 
 
 
-sudo su
+
 
 wget https://packages.microsoft.com/config/ubuntu/16.04/packages-microsoft-prod.deb
-sudo dpkg -i packages-microsoft-prod.deb
-sudo apt-get update
-sudo apt-get -y install blobfuse fuse
+dpkg -i packages-microsoft-prod.deb
+apt-get update
+apt-get -y install blobfuse fuse
 
 export MOUNTDIR=/mnt/$AZURE_CONTAINER
 export FUSETMP=/mnt/blobfusetmp
@@ -37,18 +42,18 @@ cd /
 # taken from https://rstudio.com/products/rstudio/download-server/debian-ubuntu/
 mkdir rstudiostartup
 cd rstudiostartup
-# do we need sudo for azure install scripts?
-sudo add-apt-repository ppa:marutter/rrutter
-sudo apt update
+# do we need for azure install scripts?
+add-apt-repository ppa:marutter/rrutter
+apt update
 
-sudo apt-get -y install r-base
+apt-get -y install r-base
 # I don't think we need apache2
-# sudo apt-get -y install apache2
-sudo apt-get -y install gdebi-core
+# apt-get -y install apache2
+apt-get -y install gdebi-core
 
 wget https://download2.rstudio.org/server/bionic/amd64/rstudio-server-1.3.1093-amd64.deb
 
-yes | sudo gdebi rstudio-server-1.3.1093-amd64.deb
+yes | gdebi rstudio-server-1.3.1093-amd64.deb
 
 # additional packages needed for some R packages
-sudo apt-get install jags
+apt-get install jags
