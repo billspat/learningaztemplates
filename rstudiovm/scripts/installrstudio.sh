1058ef3dae15d783fc2094efe677cbce42cb8a08
Patrick Bills <billspat@msu.edu>
2020-10-03 00:25:45 -0400

    have only admin user, fix blob fuse, add tidyverse install

--- rstudiovm/scripts/installrstudio.sh
+++ rstudiovm/scripts/installrstudio.sh
@@ -1,59 +1,84 @@
 #!/bin/bash
 
-# run this all as root
-# TODO - can we use sudo in the command in the template?
-sudo su
+# azure vm setup script.  adds blobfuse connection for current user, which must be in sudoers
 
-export NEWUSER=rstudio
-export NEWUSERPASSWORD=$1
+ #--- params 
+export USERPASSWORD=$1  # password for the admin user sent by template, which uses ssh key
 export AZURE_STORAGE_ACCOUNT=$2
 export AZURE_CONTAINER=$3
 export AZURE_STORAGE_ACCESS_KEY=$4
 
-apt-get update
-apt-get -y upgrade
+# ------
+yes $USERPASSWORD | sudo passwd $USER
 
-# add a regular linux user for Rstudio to work with 
-useradd $NEWUSER  -m
-yes $NEWUSERPASSWORD | passwd $NEWUSER
-# this is not the same as the admin user set by the Azure template, and is not in the sudoers list.  
+# TODO combine these operations with the apt-get updates below
+sudo apt-get update
+sudo apt-get -y upgrade
 
+# Linux packages needed for tidyverse and Jags R packages
+sudo apt-get -y install libxml2-dev  libssl-dev libcurl4-openssl-dev jags
 
+cd /usr/local/src
 
+#===== BLOBFUSE ======
+# check if this is true: 
+#  Just a note - blobfuse needs to store the entire file contents on the disk while the file is open. If you don't have space, things will stop working.
+# this is terrible if the container is large, then this is not a good solution for a remote disk 
 
-
-wget https://packages.microsoft.com/config/ubuntu/16.04/packages-microsoft-prod.deb
-dpkg -i packages-microsoft-prod.deb
-apt-get update
-apt-get -y install blobfuse fuse
+sudo wget https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-prod.deb
+sudo dpkg -i packages-microsoft-prod.deb
+sudo apt-get update
+sudo apt-get -y install blobfuse fuse
 
 export MOUNTDIR=/mnt/$AZURE_CONTAINER
 export FUSETMP=/mnt/blobfusetmp
-mkdir $MOUNTDIR
-chown $NEWUSER $MOUNTDIR 
-chmod a+rw $FUSETMP
 
-blobfuse /mnt/$MOUNTDIR --container-name=$AZURE_CONTAINER --tmp-path=$FUSETMP
-ln -s $MOUNTDIR /home/$NEWUSER/$AZURE_CONTAINER
-chown $NEWUSER /home/$NEWUSER/$AZURE_CONTAINER 
+# create the config file that we'll use for blob fuse
+echo -e "accountName $AZURE_STORAGE_ACCOUNT \naccountKey $AZURE_STORAGE_ACCESS_KEY \ncontainerName $AZURE_CONTAINER" > $HOME/bfconfig
 
-cd /
+export BFCONFIGFILE=/etc/blobfuse/${USER}config  
+
+sudo mkdir /etc/blobfuse  # use basedir here 
+sudo chmod a+r /etc/blobfuse
+
+sudo mv ~/bfconfig $BFCONFIGFILE
+sudo chown $USER $BFCONFIGFILE
+sudo chmod 600 $BFCONFIGFILE  # only for this user
+
+sudo mkdir -p $MOUNTDIR
+sudo chown $USER $MOUNTDIR 
+sudo chown $USER $FUSETMP 
+
+# run as current user.  that measn this mount only works with the current user
+
+blobfuse $MOUNTDIR --tmp-path=/mnt/blobfusetmp \
+   -o attr_timeout=240 -o entry_timeout=240 -o negative_timeout=120 \
+   --config-file=$BFCONFIGFILE
+
+# blobfuse /mnt/$MOUNTDIR --container-name=$AZURE_CONTAINER --tmp-path=$FUSETMP
+# add the /mnt dir as a short cut in the user home dir for easy access via rstudio
+sudo ln -s $MOUNTDIR $HOME/$AZURE_CONTAINER
+sudo chown $USER $HOME/$AZURE_CONTAINER
+
+
+
+#===== R & Rstudio ======
+
 
 # taken from https://rstudio.com/products/rstudio/download-server/debian-ubuntu/
-mkdir rstudiostartup
-cd rstudiostartup
-# do we need for azure install scripts?
-add-apt-repository ppa:marutter/rrutter
-apt update
 
-apt-get -y install r-base
-# I don't think we need apache2
-# apt-get -y install apache2
-apt-get -y install gdebi-core
+# mkdir rstudiostartup
+# cd rstudiostartup
+sudo add-apt-repository ppa:marutter/rrutter
+sudo apt update
 
-wget https://download2.rstudio.org/server/bionic/amd64/rstudio-server-1.3.1093-amd64.deb
+sudo apt-get -y install r-base
 
-yes | gdebi rstudio-server-1.3.1093-amd64.deb
+sudo apt-get -y install gdebi-core
 
-# additional packages needed for some R packages
-apt-get install jags
+sudo wget https://download2.rstudio.org/server/bionic/amd64/rstudio-server-1.3.1093-amd64.deb
+
+yes | sudo gdebi rstudio-server-1.3.1093-amd64.deb
+
+# additional dependencies for packages needed for some R packages
+Rscript -e "install.packages(c('renv','tidyverse','jagsui')"  # a package manager
