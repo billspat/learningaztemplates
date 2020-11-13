148191a0667acc4fd04b9d5d0825480e1e775dcb
Patrick Bills <billspat@msu.edu>
2020-11-13 08:57:29 -0500

    disable blob storage mounting, R install typo

--- rstudiovm/scripts/installrstudio.sh
+++ rstudiovm/scripts/installrstudio.sh
@@ -37,40 +37,40 @@ cd /usr/local/src
 #  Just a note - blobfuse needs to store the entire file contents on the disk while the file is open. If you don't have space, things will stop working.
 # this is terrible if the container is large, then this is not a good solution for a remote disk
 
-sudo wget https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-prod.deb
-sudo dpkg -i packages-microsoft-prod.deb
-sudo apt-get update
-sudo apt-get -y install blobfuse fuse
+# sudo wget https://packages.microsoft.com/config/ubuntu/20.04/packages-microsoft-prod.deb
+# sudo dpkg -i packages-microsoft-prod.deb
+# sudo apt-get update
+# sudo apt-get -y install blobfuse fuse
 
-export MOUNTDIR=/mnt/$AZURE_CONTAINER
-export FUSETMP=/mnt/blobfusetmp
+# export MOUNTDIR=/mnt/$AZURE_CONTAINER
+# export FUSETMP=/mnt/blobfusetmp
 
-# create the config file that we'll use for blob fuse
-echo -e "accountName $AZURE_STORAGE_ACCOUNT \naccountKey $AZURE_STORAGE_ACCESS_KEY \ncontainerName $AZURE_CONTAINER" > $HOME/bfconfig
+# # create the config file that we'll use for blob fuse
+# echo -e "accountName $AZURE_STORAGE_ACCOUNT \naccountKey $AZURE_STORAGE_ACCESS_KEY \ncontainerName $AZURE_CONTAINER" > $HOME/bfconfig
 
-export BFCONFIGFILE=/etc/blobfuse/${USER}config
+# export BFCONFIGFILE=/etc/blobfuse/${USER}config
 
-sudo mkdir /etc/blobfuse  # use basedir here
-sudo chmod a+r /etc/blobfuse
+# sudo mkdir /etc/blobfuse  # use basedir here
+# sudo chmod a+r /etc/blobfuse
 
-sudo mv ~/bfconfig $BFCONFIGFILE
-sudo chown $USER $BFCONFIGFILE
-sudo chmod 600 $BFCONFIGFILE  # only for this user
+# sudo mv ~/bfconfig $BFCONFIGFILE
+# sudo chown $USER $BFCONFIGFILE
+# sudo chmod 600 $BFCONFIGFILE  # only for this user
 
-sudo mkdir -p $MOUNTDIR
-sudo chown $USERID $MOUNTDIR
-sudo chown $USERID $FUSETMP
+# sudo mkdir -p $MOUNTDIR
+# sudo chown $USERID $MOUNTDIR
+# sudo chown $USERID $FUSETMP
 
-# run as current user.  that measn this mount only works with the current user
+# # run as current user.  that measn this mount only works with the current user
 
-blobfuse $MOUNTDIR --tmp-path=/mnt/blobfusetmp \
-   -o attr_timeout=240 -o entry_timeout=240 -o negative_timeout=120 \
-   --config-file=$BFCONFIGFILE
+# blobfuse $MOUNTDIR --tmp-path=/mnt/blobfusetmp \
+#    -o attr_timeout=240 -o entry_timeout=240 -o negative_timeout=120 \
+#    --config-file=$BFCONFIGFILE
 
-# blobfuse /mnt/$MOUNTDIR --container-name=$AZURE_CONTAINER --tmp-path=$FUSETMP
-# add the /mnt dir as a short cut in the user home dir for easy access via rstudio
-sudo ln -s $MOUNTDIR $HOME/$AZURE_CONTAINER
-sudo chown $USERID $HOME/$AZURE_CONTAINER
+# # blobfuse /mnt/$MOUNTDIR --container-name=$AZURE_CONTAINER --tmp-path=$FUSETMP
+# # add the /mnt dir as a short cut in the user home dir for easy access via rstudio
+# sudo ln -s $MOUNTDIR $HOME/$AZURE_CONTAINER
+# sudo chown $USERID $HOME/$AZURE_CONTAINER
 
 
 
@@ -94,6 +94,9 @@ sudo wget https://download2.rstudio.org/server/bionic/amd64/rstudio-server-1.3.1
 yes | sudo gdebi rstudio-server-1.3.1093-amd64.deb
 
 # additional dependencies for packages needed for some R packages
-Rscript -e "install.packages(c('renv','tidyverse','jagsui'))"  # a package manager
+
+Rscript -e "install.packages(c('renv','tidyverse',Rcpp','littler'))"
+Rscript -e "install.packages('jagsUI')"
+  # a package manager
 
 
